37cd3264534cd0a6ca7ea8e27487b13ed52443de
Sobhan Mohammadpour <somo@mit.edu>
2024-12-08 20:02:19 -0500

    Delete test.jl

--- test.jl
+++
@@ -1,7 +0,0 @@
-using JuMP, GLPK
-model = read_from_file("src/test_problems/sample.mps")
-set_optimizer(model, GLPK.Optimizer)
-optimize!(model)
-
-objective_value(model)
-value.(all_variables(model))
